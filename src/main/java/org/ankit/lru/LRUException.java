7ea3e0a440257eb27c889af53e0e16be1ef868de
Ankit Srivastava <ankit.srivastava@insideview.com>
2016-12-22 03:16:20 +0530

    LRU Cache working in memory

---
+++ src/main/java/org/ankit/lru/LRUException.java
@@ -0,0 +1,7 @@
+package org.ankit.lru;
+
+/**
+ * Created by ankit on 22/12/16.
+ */
+public class LRUException {
+}
---
+++ src/test/java/org/ankit/lru/LRUTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by ankit on 22/12/16.
+ */
+public class LRUTest {
+
+}
